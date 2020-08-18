68f8a73ba12ddfe802e4b5bb76a2152bceb7e2e4
Ivan Smalzer <wisprp@gmail.com>
2020-08-18 15:48:27 +0200

    Add topic/lambda permissions

--- iac/sns/sns.tf
+++ iac/sns/sns.tf
@@ -40,6 +40,14 @@ resource "aws_sns_topic_subscription" "sns-subscription" {
   endpoint  = var.endpoint.arn
 
   depends_on = [
-    aws_sns_topic.topic,
+    aws_sns_topic.topic
   ]
+}
+
+resource "aws_lambda_permission" "with_sns" {
+    statement_id = "AllowExecutionFromSNS"
+    action = "lambda:InvokeFunction"
+    function_name = var.endpoint.arn
+    principal = "sns.amazonaws.com"
+    source_arn = aws_sns_topic.topic.arn
 }
